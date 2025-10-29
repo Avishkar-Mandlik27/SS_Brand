3deed34bbbf0c1eac814ff5bd89fbe2602367d6a
Avishkar-Mandlik27 <avishkarvmandlik@gmail.com>
2025-10-29 19:40:46 +0530

    chore: push latest changes

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -10,7 +10,7 @@ const ProductGrid = () => {
   return (
     <section className="min-h-screen pt-24 pb-16 px-6 md:px-12 bg-[#FFF8E7] font-poppins">
       <h2 className="text-3xl md:text-4xl font-bold text-[#5C2C06] text-center mb-12">
-        {language === "EN" ? "Explore Our Oils" : "आमची तेलं पहा"}
+        {language === "EN" ? "Explore Our Oils" : "आमच्या शुद्ध तेलांचा अनुभव घ्या"}
       </h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
